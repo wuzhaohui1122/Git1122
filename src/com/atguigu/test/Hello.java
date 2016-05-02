bb8433f19a3e9b3e33d18e1e0e9b794994236738
wuzhaohui1122 <wuzhaohui18729371641@hotmail.com>
2016-05-02 23:15:47 +0800

    update 02

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -3,7 +3,7 @@ package com.atguigu.test;
 public class Hello {
    public static void main(String[] args) {
 	System.out.println("第一次請求.....request01");
-	   
+	System.out.println("第二次请求...request02");  
 	   
   }
 }
