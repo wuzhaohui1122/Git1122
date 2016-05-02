84ae97804e465bf8a2c2ee3c5dc3bf4402917b85
wuzhaohui1122 <wuzhaohui18729371641@hotmail.com>
2016-05-02 23:21:44 +0800

    response01

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -3,7 +3,8 @@ package com.atguigu.test;
 public class Hello {
    public static void main(String[] args) {
 	System.out.println("第一次請求.....request01");
-	System.out.println("第二次请求...request02");  
+	System.out.println("第二次请求...request02");
+	System.out.println("第一次回复....response01");
 	   
   }
 }
